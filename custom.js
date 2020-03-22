877a4e42673965a2699d6a6139b223f07ea14002
SzBa <48062979+SzBa@users.noreply.github.com>
2020-03-22 14:28:18 +0100

    .

--- custom.js
+++ custom.js
@@ -22,3 +22,32 @@ class Circle{
         q.stroke();
     }
 }
+class Point{
+    constructor(context){
+        this.context = q;
+    }
+    draw(x,y){
+        q.fillRect(x,y,2,2);
+    }
+}
+class Polygon{
+    constructor(context){
+        this.context = q;
+    }
+    draw(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5){
+        let points = [
+            [x1, y1],
+            [x2, y2],
+            [x3, y3],
+            [x4, y4],
+            [x5, y5]
+            ];
+        q.beginPath();
+        q.moveTo(points[0][0], points[0][1]);
+        for(var i=1; i<points.length; i++){
+            q.lineTo(points[i][0],points[i][1]);
+        }
+        q.closePath();
+        q.stroke();
+    }
+}
